2786b5ea1045da2639fed264f55aaed23f477930
liguolin <liguolin@>
2018-10-23 21:24:24 +0800

    replayingDecoder

--- src/main/java/org/bazinga/SimpleByteToMsgDecoder.java
+++ src/main/java/org/bazinga/SimpleByteToMsgDecoder.java
@@ -3,6 +3,7 @@ package org.bazinga;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +13,7 @@ import java.util.List;
  * @author liguolin
  * @create 2018-10-23 20:47
  **/
-public class SimpleByteToMsgDecoder extends ByteToMessageDecoder {
+public class SimpleByteToMsgDecoder extends ReplayingDecoder<Void> {
 
     private static Logger logger = LoggerFactory.getLogger(SimpleByteToMsgDecoder.class);
 
@@ -22,21 +23,9 @@ public class SimpleByteToMsgDecoder extends ByteToMessageDecoder {
 
         logger.info("SimpleByteToMsgDecoder invoke decode");
 
-        if(in.readableBytes() < 4){
-            return;
-        }
+        byte[] bytes = new byte[in.readInt()];
+        in.readBytes(bytes);
 
-        in.markReaderIndex();
-        int contextLength = in.readInt();
-
-        if(in.readableBytes() < contextLength){
-            in.resetReaderIndex();
-            return;
-        }
-
-        byte[] byteArray = new byte[contextLength];
-        in.readBytes(byteArray);
-
-        out.add(new String(byteArray));
+        out.add(new String(bytes));
     }
 }
