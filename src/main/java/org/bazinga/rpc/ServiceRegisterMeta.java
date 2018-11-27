f712b79ef41b1a1bf72241c884d1ac80d4820794
liguolin <liguolin@>
2018-11-27 15:48:18 +0800

    实行逻辑增加

--- src/main/java/org/bazinga/rpc/ServiceRegisterMeta.java
+++ src/main/java/org/bazinga/rpc/ServiceRegisterMeta.java
@@ -9,11 +9,31 @@ import java.util.List;
  **/
 public class ServiceRegisterMeta {
 
+    private String group;
+
     private String address;
 
     private String interfaceName;
 
+    private String version;
+
     private List<Method> methodList;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
     public String getInterfaceName() {
         return interfaceName;
